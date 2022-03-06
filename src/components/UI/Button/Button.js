2caef76ce8b7c8f366830154a7d3cf3efce5a4c4
Marc Medhat <marcmedhat6211@gmail.com>
2022-03-06 17:13:08 +0200

    adding styled components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import styled from "styled-components";
 
-import './Button.css';
+// import "./Button.css";
+/**
+ * button is a method on the styled object
+ * and you can just add the style between the 2 backticks
+ * this will be executed as a method behind the scenes
+ * this will return a new button component
+ */
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
+
+// const Button = (props) => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
